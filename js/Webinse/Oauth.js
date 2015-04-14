e2b2bfef30c26f1dab9c47c0f0555c6605b1c7f9
Dmitriy Perederiy <perederiy1993@yandex.ua>
2015-04-14 14:18:22 +0300

    change style Oauth

--- js/Webinse/Oauth.js
+++ js/Webinse/Oauth.js
@@ -1,10 +1,16 @@
+var ModalHeight = 515;
+var ModalWidth= 325;
+
+
+
 function PopOauthWindow(e){
     if($('ajaxLogin').value=='ajax'){
         new Ajax.Request(e.href, {
             method: 'post',
-            onSuccess: function(transport) {
+            onComplete: function(transport) {
                 var response = transport.responseText;
                 showPopup(response);
+
                 return false;
             }
         });
@@ -16,8 +22,9 @@ return true;
 };
 
 
+
 function showPopup(data) {
-    oPopup = new Window({id: 'modal_window',className: 'magento', width:400, height:515, destroyOnClose: true,
+    oPopup = new Window({id: 'modal_window',className: 'magento', width:ModalWidth, height:ModalHeight, destroyOnClose: true,
         showEffectOptions: {
         duration: 0
     },
@@ -25,11 +32,37 @@ function showPopup(data) {
             duration: 0.6
         }
     });
+
+
     oPopup.getContent().update(data);
     oPopup.setZIndex(2000);
     oPopup.showCenter(true);
-    $('modal_window_close').insert({before:'<div id="oblogka" class="mainWindow aling-central-modal" style="display: none; width: 100%;height: 100%; z-index:20000; position: absolute;margin-right: auto; margin-left: auto; background-color: #000000; opacity: 0.5"><i class="fa fa-spinner fa-pulse fa-5x" style="margin-top: 65%; color: #ffffff"></i></div>'});
+
+
+    myObserver = {
+        onShow: function(eventName, win) {
+
+            $('modal_window_close').insert({before:'<div id="oblogka" class="mainWindow aling-central-modal" style="display: none; width: 100%;height: 100%; z-index:20000; position: absolute;margin-right: auto; margin-left: auto; background-color: #000000; opacity: 0.5"><i class="fa fa-spinner fa-pulse fa-5x" style="margin-top: 65%; color: #ffffff"></i></div>'});
+            $('modal_window_minimize').remove();
+            $('modal_window_maximize').remove();
+
+            $('modal_window_content').removeAttribute('style');
+            $('modal_window_content').setStyle({
+                width: '100%',
+                height: ModalHeight+'px'
+
+            });
+
+            $('overlay_modal').observe('click',function(){
+                Windows.close('modal_window',event);
+            });
+        }
+    };
+
+    Windows.addObserver(myObserver);
 }
+
+
 
 
 function formModalValidation(e){
@@ -93,6 +126,10 @@ function ModalFormCreate(element){
     Validation.add('validate-formCreateModalEm','',function(v){
         return true;
     });
+    Validation.add('validate-formCreateModalFirstLast','Вводить имя и фамилию по данному шаблону "Имя Фамилия"',function(v){
+        return GetText($('firstnameLastname').value);
+    });
+
     if(validator.validate()) {
 
         new Ajax.Request(element.title, {
@@ -193,4 +230,9 @@ function ForgotPasswordShow(){
 function ForgotPasswordHide(){
     $('modal-forgot-password-form').hide();
     $('modal-login-form-main').show();
+}
+
+function GetText(AInputText) {
+    var VRegExp = new RegExp(/[A-Za-zА-Яа-я]{2,255} {1}[A-Za-zА-Яа-я]{2,255}/);
+    return VRegExp.test(AInputText);
 }
